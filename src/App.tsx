080b08caf014c3e2578ac3682e497b64f39a9706
[dyad] <michaelschal@gmail.com>
2025-11-06 16:08:49 +0100

    [dyad] wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Index from "./pages/Index";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import AuthService from "./services/AuthService";
+import "./globals.css";
 
 const queryClient = new QueryClient();
 
